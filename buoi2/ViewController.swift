d087990b8088c4fa760355ea24cfe28aec4d60a1
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-05-16 17:57:13 -0600

    added all

--- buoi2/ViewController.swift
+++ buoi2/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lbl: UILabel!
+    
+    @IBOutlet weak var imgHinh: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lbl.text = "Hello world !"
+        //Color for the text Hello :
+        
+        lbl.textColor = UIColor.green
+        
+        //image for background :
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "hinh.jpg")!)
     }
-
-
+    @IBAction func XULY(_ sender: Any) {
+        
+        lbl.text = " Kien Nguyen"
+    }
+    
+    @IBAction func LoadHinhLocal(_ sender: Any) {
+        imgHinh.image = UIImage(named: "hinh.jpg")
+        
+    }
+    
+    @IBAction func LoadHinhInternet(_ sender: Any) {
+        //Create URL with https  :
+        
+        // image with http changed(needed App Transport Security):
+        let u = URL(string :"http://vinakoforwarding.com/wp-content/uploads/2017/06/photoshop-anh-logo-2.jpg")
+        
+        //Create data :
+        do{
+          let d = try Data(contentsOf: u!)
+            imgHinh.image = UIImage(data: d)   // Hien thi hinh ra
+        }catch{
+            print(" Loi data !!!")
+        }
 }
-
+}
